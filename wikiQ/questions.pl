96170868081d9ea5cec7a9f954412a90b4d24fc3
Toby Burns <toby8urns@gmail.com>
2017-01-11 12:58:50 +0000

    More Exam Practice

--- wikiQ/questions.pl
+++ wikiQ/questions.pl
@@ -15,4 +15,9 @@ mylength([], 0).
 mylength([_|Xs], Y+1) :- mylength(Xs, Y).
 
 %Q5
+myreverse([], Z, Z).
+myreverse([X|Xs], Y, Z) :- myreverse(Xs, Y, [X|Z]).
+
+%Q6
+palindrome(Xs) :- myreverse(Xs, Ys, []), Xs==Ys.
 
--- wikiQ/questions.rkt
+++ wikiQ/questions.rkt
@@ -1,2 +1,47 @@
 #lang racket
-;;;Q1
+;;Q1
+(define mylast
+  (λ (xs)
+    (cond ((null? (cdr xs)) (car xs))
+          (else (mylast (cdr xs))))))
+
+;;Q2
+(define mybutlast
+  (λ (xs)
+    (cond ((null? (cddr xs)) (car xs))
+          (else (mybutlast (cdr xs))))))
+
+;;Q3
+(define elementat
+  (λ (xs x)
+    (cond ((equal? x 1) (car xs))
+          (else (elementat (cdr xs) (- x 1))))))
+
+;;Q4
+(define mylength
+  (λ (xs)
+    (cond ((null? (cdr xs)) 1)
+          (else (+ 1 (mylength (cdr xs)))))))
+
+;;Q5
+(define myreverse
+  (λ (xs)
+    (cond ((null? (cdr xs)) `(,(car xs)))
+          (else `(,@(myreverse (cdr xs)) ,(car xs))))))
+
+;;Q6
+(define palindrome
+  (λ (xs)
+    (equal? xs (myreverse xs))))
+
+;;Q7
+(define myflatten
+  (λ (xs)
+    `(,@(cond ((list? (car xs)) ((myflatten (car xs))))
+              (else `(,(car xs))))
+      ,@(if (null? (cdr xs)) '() (myflatten (cdr xs))))))
+
+
+
+
+
